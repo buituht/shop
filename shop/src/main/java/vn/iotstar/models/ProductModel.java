262acd10d890291866fd922f4f8c1c65ded88b99
buituht <buituht@gmail.com>
2025-09-29 12:55:43 +0700

    Create ProductModel.java

---
+++ shop/src/main/java/vn/iotstar/models/ProductModel.java
@@ -0,0 +1,71 @@
+package vn.iotstar.models;
+
+import java.sql.Timestamp;
+
+public class ProductModel {
+    private int id;
+    private int cateId;
+    private String name;
+    private double price;
+    private String image;
+    private String description;
+    private int status;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getCateId() {
+        return cateId;
+    }
+    public void setCateId(int cateId) {
+        this.cateId = cateId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public String getImage() {
+        return image;
+    }
+    public void setImage(String image) {
+        this.image = image;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public int getStatus() {
+        return status;
+    }
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
