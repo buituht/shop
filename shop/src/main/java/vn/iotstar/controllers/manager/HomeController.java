b79a15957534b0b18af80968212d35d2f31daa42
buituht <buituht@gmail.com>
2025-09-29 12:55:10 +0700

    Create HomeController.java

---
+++ shop/src/main/java/vn/iotstar/controllers/manager/HomeController.java
@@ -0,0 +1,37 @@
+package vn.iotstar.controllers.manager;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/manager/home")
+public class HomeController extends HttpServlet{
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.getRequestDispatcher("/views/manager/home.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+		
+		
+	}
+	
+	
+}
