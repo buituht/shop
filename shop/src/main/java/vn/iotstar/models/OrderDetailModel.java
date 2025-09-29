411b7924063e45e8594adfeac73ed09857d1b3e0
buituht <buituht@gmail.com>
2025-09-29 12:55:35 +0700

    Create OrderDetailModel.java

---
+++ shop/src/main/java/vn/iotstar/models/OrderDetailModel.java
@@ -0,0 +1,64 @@
+package vn.iotstar.models;
+
+public class OrderDetailModel {
+    
+    private int detailId;
+    private int orderId;
+    private int productId;
+    private String productName;
+    private int quantity;
+    private double price;
+    
+    // Constructor (Rỗng)
+    public OrderDetailModel() {}
+
+    // Getters and Setters (Bạn hãy thêm đầy đủ các getter/setter còn thiếu)
+    
+    public int getDetailId() {
+        return detailId;
+    }
+
+    public void setDetailId(int detailId) {
+        this.detailId = detailId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
