cef735879f2f751d30384e64a74fca6a98f298c0
buituht <buituht@gmail.com>
2025-10-21 10:17:15 +0700

    Update UserDaoImpl.java

--- shop/src/main/java/vn/iotstar/daos/impl/UserDaoImpl.java
+++ shop/src/main/java/vn/iotstar/daos/impl/UserDaoImpl.java
@@ -5,6 +5,9 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -12,6 +15,8 @@ import org.apache.catalina.User;
 
 import jakarta.security.auth.message.callback.PrivateKeyCallback.Request;
 import vn.iotstar.daos.UserDao;
+import vn.iotstar.models.CategoryModel;
+import vn.iotstar.models.ProductModel;
 import vn.iotstar.models.UserModel;
 import vn.iotstar.services.UserService;
 import vn.iotstar.jdbc.DBConnection;
@@ -19,7 +24,7 @@ import vn.iotstar.jdbc.DBConnection;
 
 public class UserDaoImpl implements UserDao{
 
-	//public Connection conn = ConnectMysql.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
+	
 	public Connection conn = null;
 	public PreparedStatement ps = null;
 	public ResultSet rs = null;
@@ -72,21 +77,15 @@ String query = "INSERT INTO users(email, username, fullname, password, avatar,ro
 Connection conn = DBConnection.getConnection();
 
 		try (PreparedStatement ps = conn.prepareStatement(query)){
-				//new DBConnection();
-				//conn = DBConnection.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
-			 	
-				//ps = conn.prepareStatement(sql);
-				
+							
 				ps.setString(1, email);
 				ps.setString(2, username);
 				ps.setString(3, fullname);
 				ps.setString(4, password);
-				ps.setString(5, avatar);
-				//ps.setInt(6, user.rol); 
+				ps.setString(5, avatar);				
 				ps.setInt(6, roleid);
 				ps.setString(7,phone);
-				// Get the current date and time as a Timestamp
-				//ps.setString(7,"10/10/1999");
+		
 				ps.setTimestamp(8, new java.sql.Timestamp(System.currentTimeMillis()));
 				
 		        System.out.println("Executing SQL: " + query);
@@ -106,39 +105,6 @@ Connection conn = DBConnection.getConnection();
 		return 0;
 	}
 
-
-
-	
-/*
-
-	@Override
-	public void insert(User user) {
-		String sql = "INSERT INTO users(email, username, fullname, password, avatar, roleid, phone, createddate) VALUES (?,?,?,?,?,?,?,?)";
-		
-		try {
-				new DBConnection();
-				conn = DBConnection.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
-				ps = conn.prepareStatement(sql);
-				
-				ps.setString(1, "Email");
-				ps.setString(2, user.getUsername());
-				ps.setString(3, user.getFullName());
-				ps.setString(4, user.getPassword());
-				ps.setString(5, "Avatar");
-				//ps.setInt(6, user.rol); 
-				ps.setInt(6, getRoleid());
-				ps.setString(7,"Phone");
-				// Get the current date and time as a Timestamp
-				ps.setTimestamp(8, new java.sql.Timestamp(System.currentTimeMillis()));
-				
-		        System.out.println("Executing SQL: " + user);
-		        
-				ps.executeUpdate();
-				
-		
-	} catch (Exception e) {e.printStackTrace();}
-	}
-*/	
 	
 	@Override
 	public boolean checkExistEmail(String email) {
@@ -181,6 +147,107 @@ Connection conn = DBConnection.getConnection();
 		} catch (Exception ex) {}
 		return duplicate;
 	}
+	
+	//user on admin
+	 @Override
+	    public List<UserModel> findAll() {
+	        // Query cần lấy tất cả các cột
+	     
+	        List<UserModel> list = new ArrayList<>();
+	        String sql = "SELECT id, email, userName, fullName, avatar, roleId, phone, createdDate FROM users ORDER BY id DESC";
+	        try {
+	        	conn = DBConnection.getConnection();
+				ps = conn.prepareStatement(sql);
+				
+	            rs = ps.executeQuery();
+	            while (rs.next()) {
+	                UserModel users = new UserModel();
+	                users.setId(rs.getInt("id"));
+	                users.setUserName(rs.getString("userName"));
+	                users.setEmail(rs.getString("email"));
+	                users.setFullName(rs.getString("fullName"));
+	                users.setRoleid(rs.getInt("roleid"));
+	                
+	                 
+	                list.add(users);
+	            }
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        return list;
+	    }
+	    
+	    @Override
+	    public UserModel findById(int id) {
+	        String sql = "SELECT * FROM users WHERE id = ?";
+	        UserModel user = null;
+	        System.out.println("Lay user: " + ps);
+	        try{
+	        	conn = DBConnection.getConnection();
+				ps = conn.prepareStatement(sql);
+	        	
+	        	
+	            ps.setInt(1, id);
+	            try (ResultSet rs = ps.executeQuery()) {
+	                if (rs.next()) {
+	                	user = new UserModel();
+	                	user.setId(rs.getInt("id"));
+	                	user.setUserName(rs.getString("username"));
+	                	user.setFullName(rs.getString("fullName"));
+	                	user.setEmail(rs.getString("email"));
+	                	user.setAvatar(rs.getString("avatar"));
+	                	user.setPhone(rs.getString("phone"));
+	                	user.setRoleid(rs.getInt("roleid"));
+	                }
+	            }
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        return user;
+	    }
+
+	    @Override
+	    public void insert(UserModel user) {
+	        // Query sử dụng tên cột DB: userName, fullName, passWord, roleId, createdDate
+	        String sql = "INSERT INTO users (email, userName, fullName, passWord, avatar, roleId, phone) VALUES (?, ?, ?, ?, ?, ?, ?)";
+	        
+	    }
+
+	    @Override
+	    public void update(UserModel user) {
+	       
+	        String sql = "UPDATE users SET email=? ,fullName=?, avatar=?, roleId=?, phone=? WHERE id=?";
+	        
+	        try {
+
+	        	conn = DBConnection.getConnection();
+				ps = conn.prepareStatement(sql);
+				
+				
+				
+	            ps.setString(1, user.getEmail());
+	  
+	            ps.setString(2, user.getFullName());
+	            ps.setString(3, user.getAvatar());
+	            ps.setInt(4, user.getRoleid());
+	            ps.setString(5, user.getPhone());
+	            ps.setInt(6, user.getId());
+	            
+	            ps.executeUpdate();
+	            System.out.println("Cap nhat user: " + ps);
+	           
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    @Override
+	    public void delete(int id) {
+	        String sql = "DELETE FROM users WHERE id = ?";
+	       
+	    }
+
+	//end user on admin
 	
 
 	
